834acf5d5b8876fb82f64d2d1fab919bbdfbf74b
Samuel Gómez Balderas <sierra_034@outlook.com>
2019-04-24 18:32:06 -0500

    añadido fontawesome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>permisos-docentes</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/fontawesome.css') }}">
+    <script src="{{ mix('js/fontawesome.js') }}"></script>
 </head>
 <body>
     @yield('form')
