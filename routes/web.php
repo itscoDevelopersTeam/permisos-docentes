439b7952118c4efe98f3bda5254c3e600a892892
Samuel Gómez Balderas <sierra_034@outlook.com>
2019-05-07 03:13:35 -0500

    rutas creadas

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,33 @@ Route::get('inicio-sesion', function() {
     return view('login.inicio-sesion');
 })->name('inicio');
 
-Route ::get('registrar', function() {
+Route::get('registrar', function() {
     return view('login.registrar');
 })->name('registro');
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    // Grupo de rutas para admins
+});
+
+Route::prefix('rh')->name('rh.')->group(function() {
+    // Grupo de rutas para recursos humanos
+});
+
+// Grupo de rutas para users
+Route::prefix('{username}')->name('user.')->group(function() {
+    
+    // Ruta home para cada usuario
+    Route::get('/', function($username) {
+        echo 'Home para ' . $username;
+    })->where('username', '[A-Za-z]+')->name('index');
+
+    // Devuelve vista para solicitar nuevo permiso
+    Route::get('/nuevo', function($username) {
+        echo 'Nuevo permiso para ' . $username;
+    })->where('username', '[A-Za-z]+')->name('nuevo');
+    
+    // Devuelve vista con histórico de permisos
+    Route::get('/consulta', function($username) {
+        echo 'Consulta permisos para ' . $username;
+    })->where('username', '[A-Za-z]+')->name('consultar');
+});
